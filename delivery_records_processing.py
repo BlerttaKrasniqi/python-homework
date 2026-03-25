17d0a0b72697cc3036276a58963202bb2525fdcc
BlerttaKrasniqi <blertakrasniqi261202@gmail.com>
2026-03-25 14:56:08 +0100

    completed task 1

---
+++ delivery_records_processing.py
@@ -0,0 +1,23 @@
+deliveries = [
+    {"delivery_id":1, "distance":12, "status":"completed"},
+    {"delivery_id":2, "distance":-3, "status":"completed"},
+    {"delivery_id":3, "distance":-8, "status":"pending"},
+    {"delivery_id":4, "distance":20, "status":"completed"}
+]
+
+def get_valid_deliveries(deliveries):
+    valid_deliveries = []
+    for delivery in deliveries:
+        if delivery["distance"] > 0 and delivery["status"]=="completed":
+            valid_deliveries.append(delivery)
+    return valid_deliveries
+
+if __name__ == "__main__":
+    print("Valid deliveries: ")
+    res = get_valid_deliveries(deliveries)
+    for d in res:
+        print(
+            f"Delivery ID: {d['delivery_id']} - "
+            f"Distance: {d['distance']} - "
+            f"Status: {d['status']}"
+        )
