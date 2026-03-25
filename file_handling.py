553847537c295134d20609dbc34ef4853d5517bb
BlerttaKrasniqi <blertakrasniqi261202@gmail.com>
2026-03-25 16:30:38 +0100

    function that validates a file, counts total number of words and handles empty files and missing file scenarios

---
+++ file_handling.py
@@ -0,0 +1,19 @@
+def file_validation(file_name):
+    try:
+        with open(file_name,"r") as f:
+            count_word = 0
+            non_empty_lines = 0
+            for line in f:
+                if line.strip():
+                    non_empty_lines+=1
+                    count_word+=len(line.split())
+            if count_word == 0 and non_empty_lines == 0:
+                return "The file is empty"
+            return {
+                "total words: ":count_word, "non_empty_lines":non_empty_lines
+            }
+    except FileNotFoundError:
+        return("File not found")
+    
+if __name__=="__main__":
+    print(file_validation("file.txt"))
