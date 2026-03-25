81cf3289fd828e5c43ef9306deeab2d61891a998
BlerttaKrasniqi <blertakrasniqi261202@gmail.com>
2026-03-25 15:15:03 +0100

    better version using try and except block to catch the error

--- clean_numeric_data.py
+++ clean_numeric_data.py
@@ -1,9 +1,18 @@
 values = ["50","75","NaN","100","error","25"]
 
-cleaned_values = []
-for v in values:
-    if v.isdigit():
-        cleaned_values.append(int(v))  
-print(cleaned_values)
 
 
+def get_clean_values(values):
+    cleaned_values = []
+    for value in values:
+        try:
+            cleaned_values.append(int(value))
+        except ValueError:
+            continue
+    return cleaned_values
+
+if __name__ == "__main__":
+    print("Values before cleaning the list: ")
+    print(values)
+    print("Cleaned values: ")
+    print(get_clean_values(values))
